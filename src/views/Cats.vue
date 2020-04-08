aa9665783d65b82941bfdb05d5dab157d145ce9d
Keyur Modi <keyurmodi@keyur-ma.local>
2020-04-08 14:55:59 +0800

    initial commit

---
+++ src/views/Cats.vue
@@ -0,0 +1,19 @@
+<template>
+  <b-container>
+    <h1>Cats</h1>
+    <b-table striped hover :items="cats"></b-table>
+  </b-container>
+</template>
+
+<script>
+import cats from '@/data/cats.js';
+
+export default {
+  name: 'Cats',
+  data() {
+    return {
+      cats
+    };
+  }
+};
+</script>
